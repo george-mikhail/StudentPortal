304cb025a9f7c065100d98ab831aea7cc0f3fae4
jenilee <jenilee.hsu@gmail.com>
2017-03-16 18:53:55 -0700

    updated sessions controller for password

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
 
   get "/students" => "resumes#show"
 
+  get "/experiences" => "experiences#"
+
   get "/students/edit" => "resumes#edit"
-  
   patch "/students" => "resumes#update"
 
   get "/login" => "sessions#new"
-
   post "/login" => "sessions#create"
-
   get "/logout" => "sessions#destroy"
   
 end
