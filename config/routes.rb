8073e9323580ec376042b8f30091808bf16de874
anniehswong <annie.hs.wong@gmail.com>
2017-03-16 23:16:54 -0700

    created index, edit, show and update actions for educations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
 
   get "/students" => "resumes#show"
-
   get "/students/edit" => "resumes#edit"
-  
   patch "/students" => "resumes#update"
 
+  get "/educations" => "educations#index"
+  post "/educations/new" => "educations#create"
+  get "/educations/:education_id" => "educations#show"
+  get "/educations/:education_id/edit" => "educations#edit"
+  patch "/educations/:education_id" => "educations#update"
+
   get "/login" => "sessions#new"
-
   post "/login" => "sessions#create"
-
   get "/logout" => "sessions#destroy"
   
 end
