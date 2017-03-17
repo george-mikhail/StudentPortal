6538086d85f50549e0a7173aadaec89af76abab0
Carlos <school@Carlos-MacBook-Air.local>
2017-03-16 21:10:20 -0700

    fixed bugs and fixing session id for  capstones

--- app/controllers/capstones_controller.rb
+++ app/controllers/capstones_controller.rb
@@ -1,16 +1,16 @@
 class CapstonesController < ApplicationController
   def show
 
-    id = params[:user_id]
+    id = session[:user_id]
 
-    @user = Unirest.get("http://localhost:3000/api/students/capstones/#{id}").body
+    @user = Unirest.get("http://localhost:3000/api/students/#{id}").body
     @capstones = @user['capstones']
     
   end
   def edit
 
-    id = params[:id]
-    @resume = Unirest.get("http://localhost:3000/api/students/capstones/#{id}").body
+    id = params[:capstone_id]
+    @resume = Unirest.get("http://localhost:3000/api/students/#{id}").body
     @capstones = @user['capstones']
     
   end
@@ -18,11 +18,11 @@ class CapstonesController < ApplicationController
 
   def update
 
-    id = params[:id]
+    id = params[:capstone_id]
 
-    @capstone = Unirest.patch("http://localhost:3000/api/students/capstones/#{id}", :parameters => {capstone_name: params[:capstone_name], short_description: params[:short_desctiption], website_url: params[:website_url], github_url: params[:github_url] #photo?
+    @capstone = Unirest.patch("http://localhost:3000/api/students/#{id}", :parameters => {capstone_name: params[:capstone_name], short_description: params[:short_desctiption], website_url: params[:website_url], github_url: params[:github_url] #photo?
       }).body
-    redirect_to capstone_path(@capstone['id'])
+    redirect_to '/capstones'
     
   end
 end
