252c90dcdf9b168a9a6da6c0ffe1865b2b95de3d
Carlos <school@Carlos-MacBook-Air.local>
2017-03-16 20:24:26 -0700

    added edit field and refactored capstones

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   get "/students/:id/edit" => "resumes#edit"
   patch "/students/:id" => "resumes#update"
 
-  get "/students/capstones" => "capstones#show"
-  get "/students/capstones/edit" => "capstones#edit"
-  patch "/students/capstones" => "capstones#update"
+  get "/capstones" => "capstones#show"
+  post "/capstones/new" => "capstones#create"
+  get "/capstones/:capstone_id" => "capstones#show"
+  get "/capstones/:capstone_id/edit" => "capstones#edit"
+  patch "/capstones/:capstone_id" => "capstones#update"
 
 
   get "/login" => "sessions#new"
