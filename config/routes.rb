7a7676fbcb3a2361bf6de5b32d98ad6f750b0f45
jenilee <jenilee.hsu@gmail.com>
2017-03-16 18:38:36 -0700

    update edit and logout features

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,14 @@ Rails.application.routes.draw do
 
   get "/students" => "resumes#show"
 
-
   get "/students/edit" => "resumes#edit"
-
-
+  
   patch "/students" => "resumes#update"
 
   get "/login" => "sessions#new"
 
-  
   post "/login" => "sessions#create"
 
-  get "logout" => "sessions#destroy"
+  get "/logout" => "sessions#destroy"
   
 end
